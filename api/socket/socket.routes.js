34459dceb56c9e926a157541557341d3351f8be9
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-07-22 12:14:06 +0300

    minor design touches

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -8,15 +8,15 @@ function connectSockets(io) {
             // io.emit('chat addMsg', msg)
             // emits only to sockets in the same room
             
-            io.to(socket.feedId).emit('feed update')
+            io.to(socket.boardId).emit('feed update')
         })
-        socket.on('feed board', feedId=>{
-            console.log({feedId})
-            if (socket.feedId) {
-                socket.leave(socket.feedId)
+        socket.on('feed board', boardId=>{
+            console.log({boardId})
+            if (socket.boardId) {
+                socket.leave(socket.boardId)
             }
-            socket.join(feedId)
-            socket.feedId = feedId;
+            socket.join(boardId)
+            socket.boardId = boardId;
         })
         
     })
